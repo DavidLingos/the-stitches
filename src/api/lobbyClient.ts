c38745210d6872331615189d9b56820d776b2f06
David Ling <david.ling@notino.com>
2022-04-30 22:44:34 +0200

    fix: lobby port

--- src/api/lobbyClient.ts
+++ src/api/lobbyClient.ts
@@ -1,5 +1,7 @@
 import { LobbyClient } from 'boardgame.io/client';
 
 export const lobbyClient = new LobbyClient({
-  server: `${window.location.hostname === 'localhost' ? 'http' : 'https'}://${window.location.hostname}:8080`,
+  server: `${window.location.hostname === 'localhost' ? 'http' : 'https'}://${window.location.hostname}${
+    window.location.hostname === 'localhost' ? ':8080' : ''
+  }`,
 });
