2a225c064ae3809229d1102e2e0a2d9ade0505d0
David Ling <david.ling@notino.com>
2022-04-30 22:49:24 +0200

    fix: lobby port

--- src/pages/Game/GameClient/index.ts
+++ src/pages/Game/GameClient/index.ts
@@ -3,9 +3,12 @@ import { TheStitchesGame } from '../../../game/TheStitchesGame';
 import { TheStitchesGameBoard } from '../../../game/TheStitchesGameBoard';
 import { SocketIO } from 'boardgame.io/multiplayer';
 
+const { protocol, hostname, port } = window.location;
+const server = `${protocol}//${hostname}:${port}`;
+
 export const GameClient = Client({
   game: TheStitchesGame,
   board: TheStitchesGameBoard,
-  multiplayer: SocketIO({ server: 'localhost:8000' }),
+  multiplayer: SocketIO({ server: server }),
   // debug: true,
 });
